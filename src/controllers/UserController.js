a935e8f8c0a26bc1f8e0afab81dc52e9b237fef4
duki <duki.dodier@gmail.com>
2023-06-11 21:25:09 -0300

    CRUD usuarios

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,13 +2,64 @@ import User from "../models/User";
 
 class UserController {
   async create(request, response) {
-    try{
-
+    try {
       const novoUsuario = await User.create(request.body);
-      response.json(novoUsuario)
+      response.json(novoUsuario);
+    } catch (e) {
+      response.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 
-    }catch(e){
-      response.status(400).json({errors: e.errors.map((err) => err.message)})
+  async index(request, response) {
+    try {
+      const users = await User.findAll();
+      return response.json({ users });
+    } catch (e) {
+      response.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async show(request, response) {
+    try {
+      const user = await User.findByPk(request.params.id);
+      return response.json(user);
+    } catch (e) {
+      response.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({ errors: ["ID inválido!"] });
+      }
+      const user = await User.findByPk(request.params.id);
+
+      if (!user) {
+        return response.status(400).json({ errors: ["Usuario não existe!"] });
+      }
+      const usuario_atualizado = await user.update(request.body);
+      return response.status(200).json({ usuario_atualizado });
+    } catch (e) {
+      response.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({ errors: ["ID inválido!"] });
+      }
+      const user = await User.findByPk(request.params.id);
+
+      if (!user) {
+        return response.status(400).json({ errors: ["Usuario não existe!"] });
+      }
+       await user.destroy();
+       return response.json({user})
+
+    } catch (e) {
+      return response.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
   }
 }
--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import  homeController from "../controllers/HomeController";
+import userController from "../controllers/UserController";
 
 const router = new Router();
 
